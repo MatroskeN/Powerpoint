e132842ce79aa8f1be8006c9708087998368136a
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2022-06-09 23:27:56 +0300

    update

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
-//create title
-// document.querySelector('#addQuestion').onclick = () => {
-//     document.querySelector('.slideTitle').innerText = document.querySelector('#question').value;
-// }
+
 
 //adding another option
-// document.querySelector('.addOption').onclick = () => {
-//     let option = document.createElement('div');
-//     option.className='optionItem';
-//     option.innerHTML = '<input type="text" placeholder="Вариант">'
-//     document.querySelector('.optionItems').appendChild(option);
-// }
+document.querySelector('.addOption').onclick = () => {
+    let option = document.createElement('div');
+    option.className='optionItem';
+    option.innerHTML = '<input type="text" placeholder="Вариант">'
+    document.querySelector('.optionItems').appendChild(option);
+}
 
-//create graph with data from inputs
-// document.querySelector('.createGraph').onclick = () => {
-//     let options = document.querySelectorAll('.optionItem');
-//     let box = document.querySelector('.slideGraph')
-//     while(box.firstChild){
-//         box.removeChild(box.firstChild);
-//     }
-//     for (let i = 0; i < options.length; i++){
-//         let name = options[i].querySelector('input').value;
-//         if (name){
-//             let column = document.createElement('div');
-//             column.className = 'graphColumn';
-//             column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">"+name+"</div>";
-//             document.querySelector(".slideGraph").appendChild(column);
-//         }
-//     }
-// }
 
 function NewSLide(id, title, options){
     this.id = id;
@@ -59,6 +39,8 @@ let existingSlides = document.querySelectorAll('.slideMini');
 document.querySelector('.addSlide').onclick = () => {
     addSlide();
     renewSlides();
+    rewriteContent(activeSlideId);
+    clearInputs();
 }
 
 document.querySelector('.createGraph').onclick = () => {
@@ -66,13 +48,58 @@ document.querySelector('.createGraph').onclick = () => {
         if (element.id === activeSlideId){
             element.title = document.querySelector('#question').value;
             document.querySelector('.slideTitle').innerHTML = element.title;
+            let options = document.querySelectorAll('.optionItem');
+            let box = document.querySelector('.slideGraph')
+            while(box.firstChild){
+                box.removeChild(box.firstChild);
+            }
+            for (let i = 0; i<options.length; i++){
+                let name = element.options = options[i].querySelector('input').value;
+                if (name){
+                    let column = document.createElement('div');
+                    column.className = 'graphColumn';
+                    column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">"+name+"</div>";
+                    document.querySelector(".slideGraph").appendChild(column);
+                }
+            }
         }
     })
 }
 function rewriteContent(id){
     slideList.forEach(item => {
-        if (item.id === id){
-            document.querySelector('.slideTitle').innerHTML = item.title;
+        if (item.title){
+            if (item.id === id){
+                document.querySelector('.slideTitle').innerHTML = item.title;
+                let options = document.querySelectorAll('.optionItem');
+                let box = document.querySelector('.slideGraph')
+                while(box.firstChild){
+                    box.removeChild(box.firstChild);
+                }
+                for (let i = 0; i<options.length; i++){
+                    let name = item.options;
+                    if (name){
+                        let column = document.createElement('div');
+                        column.className = 'graphColumn';
+                        column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">"+name+"</div>";
+                        document.querySelector(".slideGraph").appendChild(column);
+                    }
+                }
+            }
+        } else {
+            if (item.id === id){
+                document.querySelector('.slideTitle').innerHTML = "Вопрос"
+                let options = document.querySelectorAll('.optionItem');
+                let box = document.querySelector('.slideGraph')
+                while(box.firstChild){
+                    box.removeChild(box.firstChild);
+                }
+                for (let i = 0; i<options.length; i++){
+                    let column = document.createElement('div');
+                    column.className = 'graphColumn';
+                    column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">Имя</div>";
+                    document.querySelector(".slideGraph").appendChild(column);
+                }
+            }
         }
     })
     console.log(id);
@@ -80,14 +107,20 @@ function rewriteContent(id){
 
 function renewSlides(){
     existingSlides = document.querySelectorAll('.slideMini');
-    existingSlides.forEach( element => {
-        element.onclick = () => {
-            let activeID = element.querySelector('.number').getAttribute('data-id');
-            rewriteContent(activeID);
-        }
+
+    $(existingSlides).on("click", function (){
+        let activeID = $(this).find('.number').data('id');
+        activeSlideId = activeID;
+        rewriteContent(activeID);
+        clearInputs();
     })
 }
 
 
 renewSlides();
 
+function clearInputs(){
+    document.querySelectorAll('input').forEach( element => {
+        element.value = "";
+    })
+}
