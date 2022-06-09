ef5593a3861827a20f83747ebb438d4506247e90
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2022-06-10 01:04:05 +0300

    ui
    a little prettier

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-
-
 //adding another option
 document.querySelector('.addOption').onclick = () => {
     let option = document.createElement('div');
-    option.className='optionItem';
+    option.className = 'optionItem';
     option.innerHTML = '<input type="text" placeholder="Вариант">'
     document.querySelector('.optionItems').appendChild(option);
 }
 
 
-function NewSLide(id, title, options){
+function NewSLide(id, title, options) {
     this.id = id;
     this.title = title;
     this.options = options;
@@ -20,12 +18,12 @@ let slideList = [];
 let index = 0;
 let activeSlideId;
 
-function addSlide(){
+function addSlide() {
     let newGraph = new NewSLide(id);
     activeSlideId = id;
     let miniSlide = document.createElement('div');
     miniSlide.className = 'slideMini';
-    miniSlide.innerHTML = '<p class="number" data-id="'+id+'">'+id+'</p> <div class="pic"></div>'
+    miniSlide.innerHTML = '<p class="number" data-id="' + id + '">' + id + '</p> <div class="pic"></div>'
     document.querySelector('.slideList').appendChild(miniSlide);
     slideList[index] = newGraph;
     index++;
@@ -40,60 +38,62 @@ document.querySelector('.addSlide').onclick = () => {
     addSlide();
     renewSlides();
     rewriteContent(activeSlideId);
-    clearInputs();
 }
 
 document.querySelector('.createGraph').onclick = () => {
-    slideList.forEach( element => {
-        if (element.id === activeSlideId){
+    slideList.forEach(element => {
+        if (element.id === activeSlideId) {
             element.title = document.querySelector('#question').value;
             document.querySelector('.slideTitle').innerHTML = element.title;
-            let options = document.querySelectorAll('.optionItem');
             let box = document.querySelector('.slideGraph')
-            while(box.firstChild){
+            while (box.firstChild) {
                 box.removeChild(box.firstChild);
             }
-            for (let i = 0; i<options.length; i++){
-                let name = element.options = options[i].querySelector('input').value;
-                if (name){
+            let options = document.querySelectorAll('.optionItem');
+            element.options = [];
+            for (let i = 0; i < options.length; i++) {
+                element.options[i] = options[i].querySelector('input').value;
+                let name = element.options[i];
+                if (name) {
                     let column = document.createElement('div');
                     column.className = 'graphColumn';
-                    column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">"+name+"</div>";
+                    column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">" + name + "</div>";
                     document.querySelector(".slideGraph").appendChild(column);
                 }
             }
         }
     })
 }
-function rewriteContent(id){
+
+function rewriteContent(id) {
     slideList.forEach(item => {
-        if (item.title){
-            if (item.id === id){
+        if (item.title) {
+            if (item.id === id) {
                 document.querySelector('.slideTitle').innerHTML = item.title;
-                let options = document.querySelectorAll('.optionItem');
-                let box = document.querySelector('.slideGraph')
-                while(box.firstChild){
+                let box = document.querySelector('.slideGraph');
+                while (box.firstChild) {
                     box.removeChild(box.firstChild);
                 }
-                for (let i = 0; i<options.length; i++){
-                    let name = item.options;
-                    if (name){
+                for (let i = 0; i < item.options.length; i++) {
+                    console.log(item.options[i]);
+                    let name = item.options[i];
+                    if (name) {
                         let column = document.createElement('div');
                         column.className = 'graphColumn';
-                        column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">"+name+"</div>";
+                        column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">" + name + "</div>";
                         document.querySelector(".slideGraph").appendChild(column);
                     }
                 }
             }
         } else {
-            if (item.id === id){
+            if (item.id === id) {
                 document.querySelector('.slideTitle').innerHTML = "Вопрос"
                 let options = document.querySelectorAll('.optionItem');
                 let box = document.querySelector('.slideGraph')
-                while(box.firstChild){
+                while (box.firstChild) {
                     box.removeChild(box.firstChild);
                 }
-                for (let i = 0; i<options.length; i++){
+                for (let i = 0; i < options.length; i++) {
                     let column = document.createElement('div');
                     column.className = 'graphColumn';
                     column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">Имя</div>";
@@ -102,25 +102,19 @@ function rewriteContent(id){
             }
         }
     })
-    console.log(id);
 }
 
-function renewSlides(){
+function renewSlides() {
     existingSlides = document.querySelectorAll('.slideMini');
 
-    $(existingSlides).on("click", function (){
+    $(existingSlides).on("click", function () {
         let activeID = $(this).find('.number').data('id');
         activeSlideId = activeID;
         rewriteContent(activeID);
-        clearInputs();
+        $(existingSlides).removeClass('activeMini');
+        $(this).addClass('activeMini');
     })
 }
 
 
 renewSlides();
-
-function clearInputs(){
-    document.querySelectorAll('input').forEach( element => {
-        element.value = "";
-    })
-}
