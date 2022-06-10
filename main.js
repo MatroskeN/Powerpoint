af0382b7f7829d2c51c79dba74689269d84809ee
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2022-06-10 13:33:37 +0300

    patch
    switcher

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ document.querySelector('.addOption').onclick = () => {
 }
 
 
-function NewSLide(id, title, options) {
+function NewSLide(id, title, chart) {
     this.id = id;
     this.title = title;
-    this.options = options;
+    this.chart = chart;
 }
 
 let id = 1;
@@ -46,22 +46,47 @@ document.querySelector('.createGraph').onclick = () => {
         if (element.id === activeSlideId) {
             element.title = document.querySelector('#question').value;
             document.querySelector('.slideTitle').innerHTML = element.title;
-            let box = document.querySelector('.slideGraph')
-            while (box.firstChild) {
-                box.removeChild(box.firstChild);
-            }
-            let options = document.querySelectorAll('.optionItem');
-            element.options = [];
-            for (let i = 0; i < options.length; i++) {
-                element.options[i] = options[i].querySelector('input').value;
-                let name = element.options[i];
-                if (name) {
-                    let column = document.createElement('div');
-                    column.className = 'graphColumn';
-                    column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">" + name + "</div>";
-                    document.querySelector(".slideGraph").appendChild(column);
+            let options = {
+                chart: {
+                    type: 'bar'
+                },
+                series: [{
+                    name: 'sales',
+                    data: []
+                }],
+                xaxis: {
+                    categories: []
                 }
             }
+            let pieOptions = {
+                chart: {
+                    type: 'donut'
+                },
+                series: [],
+                labels: []
+            }
+            let box = document.querySelector('#chart');
+            while(box.firstChild){
+                box.removeChild(box.firstChild)
+            }
+            let optionsData = document.querySelectorAll('.optionItem');
+            let chart;
+            if (document.querySelector('#bar').checked){
+                for (let i = 0; i < optionsData.length; i++){
+                    options.xaxis.categories.push(optionsData[i].querySelector('input').value);
+                    options.series[0].data.push(1+i);
+                }
+                chart = new ApexCharts(document.querySelector("#chart"), options);
+            }
+            if (document.querySelector('#pie').checked){
+                for (let i = 0; i < optionsData.length; i++){
+                    pieOptions.labels.push(optionsData[i].querySelector('input').value);
+                    pieOptions.series.push(50);
+                }
+                chart = new ApexCharts(document.querySelector("#chart"), pieOptions);
+            }
+            element.chart = chart;
+            element.chart.render();
         }
     })
 }
@@ -71,34 +96,32 @@ function rewriteContent(id) {
         if (item.title) {
             if (item.id === id) {
                 document.querySelector('.slideTitle').innerHTML = item.title;
-                let box = document.querySelector('.slideGraph');
-                while (box.firstChild) {
-                    box.removeChild(box.firstChild);
+                let box = document.querySelector('#chart');
+                while(box.firstChild){
+                    box.removeChild(box.firstChild)
                 }
-                for (let i = 0; i < item.options.length; i++) {
-                    console.log(item.options[i]);
-                    let name = item.options[i];
-                    if (name) {
-                        let column = document.createElement('div');
-                        column.className = 'graphColumn';
-                        column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">" + name + "</div>";
-                        document.querySelector(".slideGraph").appendChild(column);
-                    }
-                }
+                item.chart.render();
+                // let box = document.querySelector('.slideGraph');
+                // while (box.firstChild) {
+                //     box.removeChild(box.firstChild);
+                // }
+                // for (let i = 0; i < item.options.length; i++) {
+                //     console.log(item.options[i]);
+                //     let name = item.options[i];
+                //     if (name) {
+                //         let column = document.createElement('div');
+                //         column.className = 'graphColumn';
+                //         column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">" + name + "</div>";
+                //         document.querySelector(".slideGraph").appendChild(column);
+                //     }
+                // }
             }
         } else {
             if (item.id === id) {
                 document.querySelector('.slideTitle').innerHTML = "Вопрос"
-                let options = document.querySelectorAll('.optionItem');
-                let box = document.querySelector('.slideGraph')
-                while (box.firstChild) {
-                    box.removeChild(box.firstChild);
-                }
-                for (let i = 0; i < options.length; i++) {
-                    let column = document.createElement('div');
-                    column.className = 'graphColumn';
-                    column.innerHTML = "<div class=\"value\">0%</div> <div class=\"title\">Имя</div>";
-                    document.querySelector(".slideGraph").appendChild(column);
+                let box = document.querySelector('#chart');
+                while(box.firstChild){
+                    box.removeChild(box.firstChild)
                 }
             }
         }
@@ -108,7 +131,6 @@ function rewriteContent(id) {
 
 function renewSlides() {
     existingSlides = document.querySelectorAll('.slideMini');
-
     $(existingSlides).on("click", function () {
         let activeID = $(this).find('.number').data('id');
         activeSlideId = activeID;
