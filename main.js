5033c69be7072e33a13de79aa6d3294d483550a1
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2022-06-14 16:22:10 +0300

    basics for deletion

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 window.Backend = "http://localhost:8085"
 
 // Добавить опцию
-function addOption(){
+function addOption() {
     let option = document.createElement('div');
     option.className = 'optionItem';
     option.innerHTML = '<input class="form-control" type="text" placeholder="Вариант">'
     document.querySelector('.optionItems').appendChild(option);
 }
+
 document.querySelector('.addOption').onclick = () => {
     addOption();
 }
 
 // Получение презентации по id
-async function GetPresentation(presentationID){
+async function GetPresentation(presentationID) {
     return new Promise(async (resolve, reject) => {
         await axios.get(window.Backend + '/api/presentation/' + presentationID)
             .then((res) => resolve(res.data))
@@ -21,7 +22,7 @@ async function GetPresentation(presentationID){
 }
 
 // Сохранение презентации
-async function SavePresentation(presentationID = null, activeSlide = null, presentationName = "", slides = []){
+async function SavePresentation(presentationID = null, activeSlide = null, presentationName = "", slides = []) {
     return new Promise(async (resolve, reject) => {
         await axios.post(window.Backend + '/api/presentation/save', {
             name: presentationName,
@@ -56,18 +57,16 @@ async function SavePresentation(presentationID = null, activeSlide = null, prese
 }
 
 // Получить результаты для графиков
-async function GetChartsResult(id_slide, id_presentation){
+async function GetChartsResult(id_slide, id_presentation) {
     return new Promise(async (resolve, reject) => {
         await axios.post(window.Backend + '/api/results', {
-                id_slide: id_slide,
-                id_presentation: id_presentation
-            })
+            id_slide: id_slide,
+            id_presentation: id_presentation
+        })
             .then((res) => resolve(res.data))
             .catch((err) => reject(err))
     })
 }
-
-
 
 
 function NewSLide(id, question, chartsID, answers) {
@@ -77,7 +76,7 @@ function NewSLide(id, question, chartsID, answers) {
     this.answers = answers;
 }
 
-function NewAnswer(id, text){
+function NewAnswer(id, text) {
     this.id = id;
     this.text = text;
 }
@@ -94,7 +93,7 @@ function addSlide() {
     activeSlideId = id;
     let miniSlide = document.createElement('div');
     miniSlide.className = 'slideMini';
-    miniSlide.innerHTML = '<p class="number" data-id="' + id + '">' + id + '</p> <div class="pic"></div>'
+    miniSlide.innerHTML = '<div class="info"><p class="number" data-id="' + id + '">' + id + '</p> <button class="remove" data-id="' + id + '"><i class="gg-trash"></i></button></div> <div class="pic"></div>'
     document.querySelector('.slideList').appendChild(miniSlide);
     slideList[index] = newGraph;
     index++;
@@ -111,7 +110,7 @@ document.querySelector('.addSlide').onclick = () => {
     rewriteContent(activeSlideId);
     clearFields();
 }
- const presentationId = 1;
+const presentationId = 1;
 
 document.querySelector('.createGraph').onclick = () => {
     let chosenOptions = '';
@@ -129,7 +128,7 @@ document.querySelector('.createGraph').onclick = () => {
             element.question = document.querySelector('#question').value;
             document.querySelector('.slideTitle').innerHTML = element.question;
             box = document.querySelector('#chart');
-            while(box.firstChild){
+            while (box.firstChild) {
                 box.removeChild(box.firstChild)
             }
             options = {
@@ -205,47 +204,47 @@ document.querySelector('.createGraph').onclick = () => {
             }
             optionsData = document.querySelectorAll('.optionItem');
             answers = [];
-            for (let i = 0; i < optionsData.length; i++){
+            for (let i = 0; i < optionsData.length; i++) {
                 answers[i] = new NewAnswer();
                 answers[i].id = i;
                 answers[i].text = optionsData[i].querySelector('input').value;
             }
-            if (document.querySelector('#bar').checked){
-                for (let i = 0; i < optionsData.length; i++){
+            if (document.querySelector('#bar').checked) {
+                for (let i = 0; i < optionsData.length; i++) {
                     options.xaxis.categories.push(answers[i].text);
-                    options.series[0].data.push(1+i);
+                    options.series[0].data.push(1 + i);
                 }
                 chosenOptions = options;
                 chartType = 'bar';
             }
-            if (document.querySelector('#pie').checked){
-                for (let i = 0; i < optionsData.length; i++){
+            if (document.querySelector('#pie').checked) {
+                for (let i = 0; i < optionsData.length; i++) {
                     pieOptions.labels.push(answers[i].text);
-                    pieOptions.series.push(1+i);
+                    pieOptions.series.push(1 + i);
                 }
                 chosenOptions = pieOptions;
                 chartType = 'pie';
             }
-            if (document.querySelector('#area').checked){
-                for (let i = 0; i < optionsData.length; i++){
+            if (document.querySelector('#area').checked) {
+                for (let i = 0; i < optionsData.length; i++) {
                     areaOptions.xaxis.categories.push(answers[i].text);
-                    areaOptions.series[0].data.push(1+i);
+                    areaOptions.series[0].data.push(1 + i);
                 }
                 chosenOptions = areaOptions;
                 chartType = 'area';
             }
-            if (document.querySelector('#radar').checked){
-                for (let i = 0; i< optionsData.length; i++){
+            if (document.querySelector('#radar').checked) {
+                for (let i = 0; i < optionsData.length; i++) {
                     radarOptions.xaxis.categories.push(answers[i].text);
-                    radarOptions.series[0].data.push(1+i);
+                    radarOptions.series[0].data.push(1 + i);
                 }
                 chosenOptions = radarOptions;
                 chartType = 'radar';
             }
-            if (document.querySelector('#polar').checked){
-                for (let i = 0; i< optionsData.length; i++){
+            if (document.querySelector('#polar').checked) {
+                for (let i = 0; i < optionsData.length; i++) {
                     polarOptions.labels.push(answers[i].text);
-                    polarOptions.series.push(1+i);
+                    polarOptions.series.push(1 + i);
                 }
                 chosenOptions = polarOptions;
                 chartType = 'polar';
@@ -271,13 +270,12 @@ document.querySelector('.createGraph').onclick = () => {
 }
 
 
-
 function rewriteContent(id) {
     if (chart) {
         chart.destroy();
     }
     let answerInputs = document.querySelector('.optionItems');
-    while(answerInputs.firstChild){
+    while (answerInputs.firstChild) {
         answerInputs.removeChild(answerInputs.firstChild);
     }
     slideList.forEach(item => {
@@ -285,11 +283,11 @@ function rewriteContent(id) {
             if (item.id === id) {
                 document.querySelector('.slideTitle').innerHTML = item.question;
                 let box = document.querySelector('#chart');
-                while(box.firstChild){
+                while (box.firstChild) {
                     box.removeChild(box.firstChild)
                 }
 
-                for (let i = 0; i < item.answers.length; i++){
+                for (let i = 0; i < item.answers.length; i++) {
                     addOption();
                 }
                 let chosenOptions = '';
@@ -364,38 +362,38 @@ function rewriteContent(id) {
                         opacity: 0.8
                     }
                 }
-                if (item.chartsID === 'bar'){
-                    for (let i = 0; i < item.answers.length; i++){
+                if (item.chartsID === 'bar') {
+                    for (let i = 0; i < item.answers.length; i++) {
                         options.xaxis.categories.push(item.answers[i].text);
-                        options.series[0].data.push(1+i);
+                        options.series[0].data.push(1 + i);
                     }
                     chosenOptions = options;
                 }
-                if (item.chartsID === 'pie'){
-                    for (let i = 0; i < item.answers.length; i++){
+                if (item.chartsID === 'pie') {
+                    for (let i = 0; i < item.answers.length; i++) {
                         pieOptions.labels.push(item.answers[i].text);
-                        pieOptions.series.push(1+i);
+                        pieOptions.series.push(1 + i);
                     }
                     chosenOptions = pieOptions;
                 }
-                if (item.chartsID === 'area'){
-                    for (let i = 0; i < item.answers.length; i++){
+                if (item.chartsID === 'area') {
+                    for (let i = 0; i < item.answers.length; i++) {
                         areaOptions.xaxis.categories.push(item.answers[i].text);
-                        areaOptions.series[0].data.push(1+i);
+                        areaOptions.series[0].data.push(1 + i);
                     }
                     chosenOptions = areaOptions;
                 }
-                if (item.chartsID === 'radar'){
-                    for (let i = 0; i < item.answers.length; i++){
+                if (item.chartsID === 'radar') {
+                    for (let i = 0; i < item.answers.length; i++) {
                         radarOptions.xaxis.categories.push(item.answers[i].text);
-                        radarOptions.series[0].data.push(1+i);
+                        radarOptions.series[0].data.push(1 + i);
                     }
                     chosenOptions = radarOptions;
                 }
-                if (item.chartsID === 'polar'){
-                    for (let i = 0; i< item.answers.length; i++){
+                if (item.chartsID === 'polar') {
+                    for (let i = 0; i < item.answers.length; i++) {
                         polarOptions.labels.push(item.answers[i].text);
-                        polarOptions.series.push(1+i);
+                        polarOptions.series.push(1 + i);
                     }
                     chosenOptions = polarOptions;
                 }
@@ -406,7 +404,7 @@ function rewriteContent(id) {
             if (item.id === id) {
                 document.querySelector('.slideTitle').innerHTML = "Вопрос"
                 let box = document.querySelector('#chart');
-                while(box.firstChild){
+                while (box.firstChild) {
                     box.removeChild(box.firstChild)
                 }
             }
@@ -428,19 +426,28 @@ let checkActive = () => {
     existingSlides = document.querySelectorAll('.slideMini');
     existingSlides.forEach(element => {
         let thisID = $(element).find('.number').data('id');
-        if (thisID === activeSlideId){
+        if (thisID === activeSlideId) {
             $(existingSlides).removeClass('activeMini');
             $(element).addClass('activeMini');
         }
     })
 }
 
-let checkInterval = setInterval(()=> checkActive(), 100);
+let checkInterval = setInterval(() => checkActive(), 100);
 
 renewSlides();
 
-function clearFields(){
-    document.querySelectorAll('input').forEach( element => {
+function clearFields() {
+    document.querySelectorAll('input').forEach(element => {
         element.value = '';
     })
 }
+
+// document.querySelectorAll('.remove').forEach( element => {
+//     $(element).on('click', function (){
+//         slideList.splice(activeSlideId-1, 1);
+//         existingSlides = document.querySelectorAll('.slideMini');
+//         document.querySelector('.slideList').removeChild(existingSlides[activeSlideId-1]);
+//         activeSlideId = 1;
+//     })
+// })
